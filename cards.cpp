aa1ba841a89eb0e24576ac0558bd85e565e740dd
orethoion <ericsun19591827346@gmail.com>
2015-10-31 15:01:49 +0900

    Create cards.cpp

---
+++ cards.cpp
@@ -0,0 +1,79 @@
+#include "cards.h"
+
+cards::cards()
+{
+	this->clear();
+}
+
+void cards::write(int location, int number)
+{
+	this->numbers[location] = number;
+	this->called[location] = false;
+}
+void cards::print()
+{
+	for (int i = 0; i < 25; i++)
+	{
+		cout << left << setw(5) << this->numbers[i];
+		if ((i + 1) % 5 == 0) 
+		{
+			cout << "               ";
+			for (int ii = 0; ii < 5; ii++)
+			{
+				if (!this->called[i + ii - 4]) 
+					cout << left << setw(5) << "O"; 
+				else 
+					cout << left << setw(5) << "X";
+			}
+			cout << endl;
+		}
+	}
+	cout << endl;
+}
+bool cards::check()
+{
+	bool call;
+	for (int i = 0; i < 5; i++)
+	{
+		call = this->called[i + 00] 
+			== this->called[i + 05] 
+			== this->called[i + 10] 
+			== this->called[i + 15] 
+			== this->called[i + 20]
+			== true 
+			|| this->called[i * 5 + 0]
+			== this->called[i * 5 + 1]
+			== this->called[i * 5 + 2]
+			== this->called[i * 5 + 3]
+			== this->called[i * 5 + 4]
+			== true ? true : false; 
+		if (call) return true;
+	}
+	call = this->called[0]
+		== this->called[6]
+		== this->called[12]
+		== this->called[18]
+		== this->called[24]
+		== true
+		|| this->called[4]
+		== this->called[8]
+		== this->called[12]
+		== this->called[16]
+		== this->called[20]
+		== true ? true : false;
+	return call;
+}
+void cards::mark(int mark)
+{
+	for (int i = 0; i < 25; i++)
+		if (this->numbers[i] == mark)
+			this->called[i] = true;
+}
+void cards::clear()
+{
+	for (int i = 0; i < 25; i++)
+	{
+		this->numbers[i] = 0;
+		this->called[i] = false;
+	}
+}
