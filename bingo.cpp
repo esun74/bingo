fc5202e79ff2f6f934f9cc0fc356d9a9f346cb14
orethoion <ericsun19591827346@gmail.com>
2015-10-31 15:00:51 +0900

    Update bingo.cpp

--- bingo.cpp
+++ bingo.cpp
@@ -1,1 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include "cards.h"
+using namespace std;
 
+void usefile();
+void userentry();
+
+int main()
+{
+	char input;
+	cout << "Do you want to use a file? Y/N" << endl;
+	cin >> input;
+
+	if (toupper(input) == 'Y')
+		usefile();
+	else
+		userentry();
+	return 0;
+}
+
+void usefile()
+{
+	system("cls");
+	int thing;
+	cards data;
+	ifstream infile;
+	infile.open("cards.txt");
+	if (infile.fail())
+		cout << "Could not open file!" << endl;
+	else
+	{
+		int location = 0;
+		while (infile >> thing)
+		{
+			data.write(location, thing);
+			location++;
+		}
+	}
+	infile.close();
+}
+
+void userentry()
+{
+	system("cls");
+	cards data;
+	int thing, location = 0;
+	cout << "Enter card numbers" << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		data.print();
+		cout << "Enter line " << i + 1 << endl;
+		for (int ii = 0; ii < 5; ii++)
+		{
+			cin >> thing;
+			data.write(location, thing);
+			location++;
+		}
+		system("cls");
+	}
+	data.print();
+}
